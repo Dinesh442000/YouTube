705259f5abe02bf619b6382170d6553d49ad9a57
Dinesh442000 <dchoudhary442000@gmail.com>
2023-09-24 20:06:37 +0530

    push

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactsed
+          Learn Reactsedvf
         </a>
       </header>
     </div>
