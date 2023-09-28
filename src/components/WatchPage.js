8e51dc717c2469d456f569491d7ca75f3293968e
Dinesh442000 <dchoudhary442000@gmail.com>
2023-09-28 20:47:55 +0530

    infinite comments

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu, toggleMenu } from "../Utils/appSlice";
 import { useParams, useSearchParams } from "react-router-dom";
+import Comments from "./Comments";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
@@ -11,17 +12,20 @@ const WatchPage = () => {
     dispatch(closeMenu());
   }, []);
   return (
-    <div className="px-24">
-      <iframe
-        className="rounded-lg"
-        width="1300"
-        height="600"
-        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowFullScreen
-      ></iframe>
+    <div className=" flex-col">
+      <div className=" px-24">
+        <iframe
+          className="rounded-lg"
+          width="1300"
+          height="600"
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <Comments />
     </div>
   );
 };
